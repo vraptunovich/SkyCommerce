a7845c49812c1abf984300ea7654b96a265fbcdb
vladimir_raptunovich <vladimirraptunovich@coherentsolution.com>
2025-11-21 11:56:50 +0100

    main: - added core logic for online store

--- src/main/java/com/rvk/skycommerce/api/error/ApiErrorResponse.java
+++ src/main/java/com/rvk/skycommerce/api/error/ApiErrorResponse.java
@@ -1,7 +1,6 @@
 package com.rvk.skycommerce.api.error;
 
 import lombok.Builder;
-import lombok.Value;
 
 import java.time.Instant;
 import java.util.List;
@@ -10,10 +9,7 @@ import java.util.List;
 public record ApiErrorResponse(Instant timestamp, int status, String error, String message, String path,
                                List<FieldError> fieldErrors) {
 
-    @Value
     @Builder
-    public static class FieldError {
-        String field;
-        String message;
+    public record FieldError(String field, String message) {
     }
 }
